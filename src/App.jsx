ea3d810b5d8e680735f227bedf8d7ce2327d17e5
Ivan <igoyzuetap@gmail.com>
2026-06-11 13:36:46 -0400

    fix navigation react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 import Bienvenida from "./Bienvenida";
 import Historia from "./Historia";
@@ -8,46 +8,50 @@ import Feriado from "./Feriado";
 function App() {
   return (
     <>
-    <h1 className="text-center mt-4">
-  Dia del trabajador boliviano
-</h1>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div className="container-fluid">
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" href="/">
-           <h4>Bienvenida</h4> 
-          </a>
-        </li>
+      <h1 className="text-center mt-4">
+        Dia del trabajador boliviano
+      </h1>
 
-        <li className="nav-item">
-          <a className="nav-link active" href="/historia">
-            <h4>Historia</h4>
-          </a>
-        </li>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
 
-        <li className="nav-item">
-          <a className="nav-link active" href="/personajes">
-            <h4>Personajes</h4>
-          </a>
-        </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/">
+                  <h4>Bienvenida</h4>
+                </Link>
+              </li>
 
-        <li className="nav-item">
-          <a className="nav-link active" href="/feriado">
-            <h4>Feriado</h4>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    <Routes>
-      <Route path="/" element={<Bienvenida />} />
-      <Route path="/historia" element={<Historia />} />
-      <Route path="/personajes" element={<Personajes />} />
-      <Route path="/feriado" element={<Feriado />} />
-    </Routes>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/historia">
+                  <h4>Historia</h4>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link active" to="/personajes">
+                  <h4>Personajes</h4>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link active" to="/feriado">
+                  <h4>Feriado</h4>
+                </Link>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Bienvenida />} />
+        <Route path="/historia" element={<Historia />} />
+        <Route path="/personajes" element={<Personajes />} />
+        <Route path="/feriado" element={<Feriado />} />
+      </Routes>
     </>
   );
 }
